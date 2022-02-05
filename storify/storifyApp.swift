17b6bf6eeafd3376358823992b5ea9350a90c5c2
Ash Chez <asherchez@gmail.com>
2022-02-05 10:12:56 +0000

    Initial Commit

---
+++ storify/storifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  storifyApp.swift
+//  storify
+//
+//  Created by Ash C on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct storifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
